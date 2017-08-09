a72214265e661e8f19d508372fc25b29007a246b
Mohanapriya1776 <30790958+Mohanapriya1776@users.noreply.github.com>
2017-08-09 13:16:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,31 @@
 console.log('Loaded!');
 
+//Counter Code
+
+var button=document.getElementById('counter');
+
+button.onclick=function(){
+    
+    var request=new HMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        if(request.readyState===HMLHttpRequest.DONE)
+        {
+            //Take some action
+            
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+                
+            }
+            
+        }
+        //Not done yet
+    };
+    //Make the request
+    
+    request.open('GET','http://mohanapriyasubramaniam.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};
