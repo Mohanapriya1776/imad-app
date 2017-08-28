a809244b81a24984db5dd0211301df2fbbbe079e
Mohanapriya1776 <30790958+Mohanapriya1776@users.noreply.github.com>
2017-08-28 11:43:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,12 +70,34 @@ function hash(input,salt)
    var key = crypto.pbkdf2Sync('input', 'salt', 100000, 512, 'sha512');
    return key.toString('hex');
 }
-
+ 
+//hash->to secure password
 app.get('/hash/:input',function(req,res)
 {
     var hashedString=hash(req.params.input,"this-is-some-random-string");
     res.send(hashedString);
 });
+
+
+app.get('create-user',function(req,res)
+{
+   var username=req.body.username;
+   var password=req.body.password;
+   var salt=crypto.randomBytes(128).toString('hex');
+   var dbString=hash(password,salt);
+   pool.query('INSERT INTO hash(username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+         if(err)
+        {
+            res.status(500).send(err.toString());
+            
+        }
+        else
+        {
+            res.send("User Successfully Created"+username);
+        }
+   });
+});
+
 
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
