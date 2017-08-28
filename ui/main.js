63691ee8f8b03bfa2dc8c77473cc0770ce21e2dd
Mohanapriya1776 <30790958+Mohanapriya1776@users.noreply.github.com>
2017-08-28 15:28:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -80,7 +80,7 @@ submit.onclick=function()
 document.ready(function(){
     
     var request=new XMLHttpRequest();
-      request.open('POST','http://mohanapriyasubramaniam.imad.hasura-app.io/create-user',true);
+      request.open('GET','http://mohanapriyasubramaniam.imad.hasura-app.io/create-user',true);
       request.send(JSON.stringify({username:"John Rambo", password:"pwd"}));
 
     request.setRequestHeader('Content-Type', 'application/json');
